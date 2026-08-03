7b02e2b4431ae5c1ff5206d79d5767dcfc588ecc
Tommy Muzzolon <muzzolontomas@gmail.com>
2026-08-03 16:27:37 -0300

    aca estan los ejercicios de grafos pendientes

---
+++ src/main/java/ar/edu/uns/cs/ed/tdas/Ejercicios/Ejercicio_2.java
@@ -0,0 +1,46 @@
+package ar.edu.uns.cs.ed.tdas.Ejercicios;
+
+import ar.edu.uns.cs.ed.tdas.tdacola.ColaConArreglo;
+import ar.edu.uns.cs.ed.tdas.tdacola.Queue;
+import ar.edu.uns.cs.ed.tdas.tdagrafo.Edge;
+import ar.edu.uns.cs.ed.tdas.tdagrafo.Graph;
+import ar.edu.uns.cs.ed.tdas.tdagrafo.Vertex;
+import ar.edu.uns.cs.ed.tdas.tdamapeo.Map;
+import ar.edu.uns.cs.ed.tdas.tdamapeo.MapeoConLista;
+
+public class Ejercicio_2<V, E> {
+    public int caminoMasCorto(Graph<V, E> g, Vertex<V> v, Vertex<V> d){
+        int cont = 0;
+        if(v.equals(d)){
+            return cont;
+        }
+        else{
+            Map<Vertex<V>, Integer> marca = new MapeoConLista<>();
+
+            cont = BFS(g, v, d, marca);
+        }
+        return cont;
+    }
+
+
+    private int BFS(Graph<V, E> g, Vertex<V> v, Vertex<V> d, Map<Vertex<V>, Integer> marca) {
+        Queue<Vertex<V>> cola = new ColaConArreglo<>();
+        cola.enqueue(v);
+        marca.put(v, 0);
+        while(!cola.isEmpty()){
+            Vertex<V> actual = cola.dequeue();
+            int dist = marca.get(actual);
+            if(actual.equals(d)){
+                return dist;
+            }
+            for(Edge<E> e : g.incidentEdges(actual)){
+                Vertex<V> vecino = g.opposite(actual, e);
+                if(marca.get(vecino) == null){
+                    marca.put(vecino, dist + 1);
+                    cola.enqueue(vecino);
+                }
+            }
+        }
+        return -1; //no hay camino entre v y d
+    }
+}
---
+++ src/main/java/ar/edu/uns/cs/ed/tdas/tdacola/ColaConArreglo.java
@@ -0,0 +1,75 @@
+package ar.edu.uns.cs.ed.tdas.tdacola;
+
+import ar.edu.uns.cs.ed.tdas.excepciones.EmptyQueueException;
+
+public class ColaConArreglo<E> implements Queue<E>{
+	
+	protected E[] arreglo;
+	protected int frente;
+	protected int fondo;
+	protected int cantidad;
+	
+	public ColaConArreglo() {
+		arreglo= (E[]) new Object [10]; 
+		frente=0;
+		fondo=0;
+		cantidad=0;
+	}
+        @Override
+	public int size() {
+		return cantidad;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return cantidad==0;
+	}
+
+	@Override
+	public E front() {
+		if(cantidad==0)
+			throw new EmptyQueueException("");
+		return arreglo[frente];
+	}
+
+	@Override
+	public void enqueue(E element) {
+		if(cantidad==arreglo.length) {
+			agrandarArreglo();
+		}
+		arreglo[fondo]=element;
+		fondo++;
+		cantidad++;
+	}
+	
+	public void agrandarArreglo() {
+		int nuevoTamano= arreglo.length*2;
+		E[] nuevo= (E []) new Object [nuevoTamano];
+		
+		for(int i=0; i<cantidad;i++)
+			nuevo[i]=arreglo[i];
+		
+		arreglo=nuevo;
+	}
+	
+        @Override
+	public E dequeue() {
+		if(cantidad==0) {
+			throw new EmptyQueueException("no se puede eliminar de una cola vacia");
+		}	
+		
+		E toRet=arreglo[frente];
+		arreglo[frente]=null;
+		frente++;
+		cantidad--;
+		
+		/* desde afuera for(int i=0; i<cantidad;i++) {
+			arreglo[i]=arreglo[i+1];
+		}*/
+		
+	return toRet;
+	}
+
+	
+}
+
